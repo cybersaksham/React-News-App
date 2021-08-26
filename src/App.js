df458e8df40047b08cd2b970ec2a83d8be1425b9
Saksham <2020UCP1801@mnit.ac.in>
2021-08-26 20:27:09 +0530

    news component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import News from "./Components/News";
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <News />
       </div>
     );
   }
