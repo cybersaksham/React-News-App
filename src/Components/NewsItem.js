20941c1fe754d9226afa8ba10928013c3069d96b
Saksham <2020UCP1801@mnit.ac.in>
2021-08-26 20:48:56 +0530

    null handled

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,11 +6,25 @@ export class NewsItem extends Component {
 
     return (
       <div className="card" style={{ width: "18rem" }}>
-        <img src={item.urlToImage} className="card-img-top" alt="..." />
+        <img
+          src={
+            item.urlToImage
+              ? item.urlToImage
+              : "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bm90JTIwZm91bmR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          }
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
-          <h5 className="card-title">{item.title}</h5>
-          <p className="card-text">{item.description}</p>
-          <a href={item.url} target="_blank" className="btn btn-sm btn-primary">
+          <h5 className="card-title">{item.title ? item.title : "No Title"}</h5>
+          <p className="card-text">
+            {item.description ? item.description : "No Description"}
+          </p>
+          <a
+            href={item.url ? item.url : "/"}
+            target="_blank"
+            className="btn btn-sm btn-primary"
+          >
             Read More
           </a>
         </div>
