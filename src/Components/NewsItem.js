4a9b9769242a86259042a563cc6c714186ef5909
Saksham <2020UCP1801@mnit.ac.in>
2021-08-26 21:09:41 +0530

    card height fixed

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,8 +5,8 @@ export class NewsItem extends Component {
     let { item } = this.props;
 
     return (
-      <div className="card" style={{ width: "18rem" }}>
-        <div class="card-header">
+      <div className="card" style={{ width: "18rem", height: "28rem" }}>
+        <div className="card-header">
           {item.source.name ? item.source.name : "Unknown"}
         </div>
         <img
@@ -15,23 +15,29 @@ export class NewsItem extends Component {
               ? item.urlToImage
               : "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bm90JTIwZm91bmR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
           }
+          style={{ height: "150px" }}
           className="card-img-top"
           alt="..."
         />
         <div className="card-body">
-          <h5 className="card-title">{item.title ? item.title : "No Title"}</h5>
+          <h5 className="card-title">
+            {item.title ? item.title.slice(0, 40) + "..." : "No Title"}
+          </h5>
           <p className="card-text">
-            {item.description ? item.description : "No Description"}
+            {item.description
+              ? item.description.slice(0, 80) + "..."
+              : "No Description"}
           </p>
           <a
             href={item.url ? item.url : "/"}
             target="_blank"
+            rel="noreferrer"
             className="btn btn-sm btn-primary"
           >
             Read More
           </a>
         </div>
-        <div class="card-footer text-muted">{item.publishedAt}</div>
+        <div className="card-footer text-muted">{item.publishedAt}</div>
       </div>
     );
   }
