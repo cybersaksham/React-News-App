57e16c6760fd4b96749fd610edd907d09b77e596
Saksham <2020UCP1801@mnit.ac.in>
2021-08-27 15:38:12 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
 
 export class App extends Component {
+  getNewsComponent = (cat) => (
+    <News pageSize={10} country={"in"} category={cat} />
+  );
+
   render() {
     return (
-      <div>
+      <Router>
         <Navbar />
-        <News pageSize={10} />
-      </div>
+        <Switch>
+          <Route exact path="/">
+            {this.getNewsComponent("general")}
+          </Route>
+          <Route exact path="/business">
+            {this.getNewsComponent("business")}
+          </Route>
+          <Route exact path="/entertainment">
+            {this.getNewsComponent("entertainment")}
+          </Route>
+          <Route exact path="/health">
+            {this.getNewsComponent("health")}
+          </Route>
+          <Route exact path="/science">
+            {this.getNewsComponent("science")}
+          </Route>
+          <Route exact path="/sports">
+            {this.getNewsComponent("sports")}
+          </Route>
+          <Route exact path="/technology">
+            {this.getNewsComponent("technology")}
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
