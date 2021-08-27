c7a983b8170ec8ef72a5458bdf3e0ee6b0953d75
Saksham <2020UCP1801@mnit.ac.in>
2021-08-27 10:32:35 +0530

    sticky navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             News
